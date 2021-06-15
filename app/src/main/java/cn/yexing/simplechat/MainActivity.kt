b44d38134234aea6d149657d11c9d983a6a7dff6
Terry <1492465117@qq.com>
2021-06-15 15:30:31 +0800

    this is a first commit.

---
+++ app/src/main/java/cn/yexing/simplechat/MainActivity.kt
@@ -0,0 +1,12 @@
+package cn.yexing.simplechat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cn.jianliao.simplechat.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
